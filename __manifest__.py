9335c6a8302fed0e7bbef587a137e041cbdea460
asartorio <alejandro.sartorio@gmail.com>
2023-09-13 00:55:09 -0300

    init

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+{
+    "name": "Button No Save",
+    "author": "Ngasturi",
+    "version": "14.0.1.0.0",
+    "category": "",
+    "website": "https://en.ngasturi.id",
+    "summary": "",
+    "description": """
+            
+        """,
+    "depends": [
+        "base",
+        "web"
+    ],
+    "data": [
+        # "views/assets.xml",
+    ],
+    "assets": {
+        'web.assets_backend': [
+            'button_no_save/static/src/js/button_no_save.js',
+        ]
+    },
+    "qweb": [
+    ],
+    "license": 'LGPL-3', 
+    "installable": True 
+}
